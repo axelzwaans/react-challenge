9df77ca7cd4186e5f794e1cf3d3d5d012202b702
Axel Zwaans <axelzwaans@hotmail.com>
2023-01-20 17:06:26 +0000

    Complete class components challenge

---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import css from "./css/Sidebar.module.css";
+
+function SideBar() {
+    return (
+        <div className={css.sidebar}>
+            <a>My Photos</a>
+            <a>My Illustrations</a>
+            <a>My Paintings</a>
+        </div>
+        )}
+
+export default SideBar;
